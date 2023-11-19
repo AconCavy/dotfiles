fc236c8d6b5b09d3d652f34bf8c39bcc5ea8215b
AconCavy <aconcavy@gmail.com>
2023-11-20 06:05:41 +0900

    Rename the setup files from init

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,6 @@
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 
 case "$(uname -s)" in
-    "Darwin") "$SCRIPT_PATH/init_mac.sh" ;;
+    "Darwin") "$SCRIPT_PATH/setup_mac.sh" ;;
     "Linux") echo "todo" ;;
 esac
