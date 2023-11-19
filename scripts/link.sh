e9b7daedaa481cdb2b127cf379a657a34cbbeeb6
AconCavy <aconcavy@gmail.com>
2023-11-20 05:41:52 +0900

    Update link and unlink to be able to use os specific dotfiles

--- scripts/link.sh
+++ scripts/link.sh
@@ -2,35 +2,44 @@
 
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 ROOT_PATH=$(dirname $SCRIPT_PATH)
-DOTFILES_PATH="$ROOT_PATH/dotfiles"
+DOTFILES_SHARED_PATH="$ROOT_PATH/dotfiles"
 DOTBACKUP_PATH="$HOME/.dotbackup"
+
+DOTFILES_OS_PATH=""
+case "$(uname -s)" in
+    "Darwin") DOTFILES_OS_PATH="$ROOT_PATH/dotfiles_mac" ;;
+    "Linux") echo "todo" ;;
+esac
 
 # create the backup directory if it doesn't exist
 test -d $DOTBACKUP_PATH || mkdir $DOTBACKUP_PATH
 
-for dotfile in $(find $DOTFILES_PATH); do
-  if [ $dotfile == $DOTFILES_PATH ]; then
-    continue
-  fi
-
-  TARGET="${dotfile##"$DOTFILES_PATH/"}"
-  HOME_TARGET="$HOME/$TARGET"
-  BACKUP_TARGET="$DOTBACKUP_PATH/$TARGET"
-
-  if [ -d $dotfile ]; then
-    # create the directory to home
-    test -d $HOME_TARGET || mkdir $HOME_TARGET
-    test -d $BACKUP_TARGET || mkdir $BACKUP_TARGET
-  fi
-
-  if [ -f $dotfile ]; then
-    # move the dotfile to the backup directory if it is not symlink
-    if [ -e $HOME_TARGET ] && [ ! -h $HOME_TARGET ]; then
-      mv $HOME_TARGET $BACKUP_TARGET
+for dotfiles_path in $DOTFILES_SHARED_PATH $DOTFILES_OS_PATH; do
+  for dotfile in $(find $dotfiles_path); do
+    if [ $dotfile == $dotfiles_path ]; then
+      continue
     fi
-
-    # create the symlink to the home directory
-    ln -snf $dotfile $HOME_TARGET
-  fi
+  
+    TARGET="${dotfile##"$dotfiles_path/"}"
+    HOME_TARGET="$HOME/$TARGET"
+    BACKUP_TARGET="$DOTBACKUP_PATH/$TARGET"
+  
+    if [ -d $dotfile ]; then
+      # create the directory to home
+      test -d $HOME_TARGET || mkdir $HOME_TARGET
+      test -d $BACKUP_TARGET || mkdir $BACKUP_TARGET
+    fi
+  
+    if [ -f $dotfile ]; then
+      # move the dotfile to the backup directory if it is not symlink
+      if [ -e $HOME_TARGET ] && [ ! -h $HOME_TARGET ]; then
+        mv $HOME_TARGET $BACKUP_TARGET
+      fi
+  
+      # create the symlink to the home directory
+      ln -snf $dotfile $HOME_TARGET
+    fi
+  
+  done
 
 done
--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -2,29 +2,38 @@
 
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 ROOT_PATH=$(dirname $SCRIPT_PATH)
-DOTFILES_PATH="$ROOT_PATH/dotfiles"
+DOTFILES_SHARED_PATH="$ROOT_PATH/dotfiles"
 DOTBACKUP_PATH="$HOME/.dotbackup"
+
+DOTFILES_OS_PATH=""
+case "$(uname -s)" in
+    "Darwin") DOTFILES_OS_PATH="$ROOT_PATH/dotfiles_mac" ;;
+    "Linux") echo "todo" ;;
+esac
 
 # create the backup directory if it doesn't exist
 test -d $DOTBACKUP_PATH || mkdir $DOTBACKUP_PATH
 
-for dotfile in $(find $DOTFILES_PATH); do
-  if [ $dotfile == $DOTFILES_PATH ]; then
-    continue
-  fi
-
-  TARGET="${dotfile##"$DOTFILES_PATH/"}"
-  HOME_TARGET="$HOME/$TARGET"
-  BACKUP_TARGET="$DOTBACKUP_PATH/$TARGET"
-
-  if [ -f $dotfile ]; then
-    # delete the symlink from the home directory
-    unlink $HOME_TARGET
-
-    # move the dotfile from the backup
-    if [ -e $BACKUP_TARGET ]; then
-      mv $BACKUP_TARGET $HOME_TARGET
+for dotfiles_path in $DOTFILES_SHARED_PATH $DOTFILES_OS_PATH; do
+  for dotfile in $(find $dotfiles_path); do
+    if [ $dotfile == $dotfiles_path ]; then
+      continue
     fi
-  fi
+  
+    TARGET="${dotfile##"$dotfiles_path/"}"
+    HOME_TARGET="$HOME/$TARGET"
+    BACKUP_TARGET="$DOTBACKUP_PATH/$TARGET"
+  
+    if [ -f $dotfile ]; then
+      # delete the symlink from the home directory
+      unlink $HOME_TARGET
+  
+      # move the dotfile from the backup
+      if [ -e $BACKUP_TARGET ]; then
+        mv $BACKUP_TARGET $HOME_TARGET
+      fi
+    fi
+  
+  done
 
 done
