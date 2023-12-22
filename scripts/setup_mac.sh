a3bc20ea344f372bd0967ad794440e097496de8b
AconCavy <aconcavy@gmail.com>
2023-12-22 21:41:58 +0900

    Modify setup_mac.sh

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
+ROOT_PATH=$(dirname $SCRIPT_PATH)
 
 /bin/bash "$SCRIPT_PATH/defaults_mac.sh"
 
@@ -9,3 +10,4 @@ xcode-select --install
 
 # install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew bundle --file "$ROOT_PATH/dotfiles_mac/.Brewfile"
