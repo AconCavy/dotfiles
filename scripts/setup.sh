5ae9e82d6fb4223c158bb70f2ce994b64292a23a
AconCavy <aconcavy@gmail.com>
2024-01-06 19:00:26 +0900

    Update the setup.sh to move defaults_mac.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,11 +10,34 @@ notice() {
   echo "[notice] $1"
 }
 
+defaults_mac() {
+  notice "Setup keyboard settings"
+  notice "Disable auto capitalization"
+  defaults wirte NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+  notice "Setup finder settings"
+  notice "Ignore .DS_Store in the Network storage"
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+  notice "Ignore .DS_Store in the USB storage"
+  defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+  notice "Show files with all extensions"
+  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+  notice "Show hidden files"
+  defaults write com.apple.finder AppleShowAllFiles -bool true
+
+  notice "Reset caches"
+  killall Finder
+  killall Dock
+}
+
 setup_mac() {
   notice "Begin to setup for mac"
 
   notice "Setup the default values"
-  /bin/bash "$SCRIPT_PATH/defaults_mac.sh"
+  defaults_mac
 
   notice "Install Xcode Commandline Tools"
   xcode-select --install
