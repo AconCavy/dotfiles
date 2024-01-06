6125da381795df07fdc4c237405301d2c09d1f36
AconCavy <aconcavy@gmail.com>
2024-01-06 18:41:14 +0900

    Update the setup.sh to extract a functions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,15 @@
 
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 
+setup_mac() {
+  "$SCRIPT_PATH/setup_mac.sh"
+}
+
+setup_linux() {
+  echo "todo"
+}
+
 case "$(uname -s)" in
-    "Darwin") "$SCRIPT_PATH/setup_mac.sh" ;;
-    "Linux") echo "todo" ;;
+    "Darwin") setup_mac ;;
+    "Linux") setup_linux ;;
 esac
