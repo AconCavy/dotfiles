1adab79089aa3c29c170f632b1e055408def9e96
AconCavy <aconcavy@gmail.com>
2024-01-06 18:46:49 +0900

    Update the setup.sh to add notice func

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,17 @@
 
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 
+notice() {
+  echo "[notice] $1"
+}
+
 setup_mac() {
+  notice "Begin to setup for mac"
   "$SCRIPT_PATH/setup_mac.sh"
 }
 
 setup_linux() {
+  notice "Begin to setup for linux"
   echo "todo"
 }
 
