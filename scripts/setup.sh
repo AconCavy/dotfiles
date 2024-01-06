8cb162c1ddb235a01b9db50f93da46009e60b05a
AconCavy <aconcavy@gmail.com>
2024-01-06 18:51:22 +0900

    Update the setup.sh to move setup_mac.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
+ROOT_PATH=$(dirname $SCRIPT_PATH)
 
 notice() {
   echo "[notice] $1"
@@ -8,7 +9,18 @@ notice() {
 
 setup_mac() {
   notice "Begin to setup for mac"
-  "$SCRIPT_PATH/setup_mac.sh"
+
+  notice "Setup the default values"
+  /bin/bash "$SCRIPT_PATH/defaults_mac.sh"
+
+  notice "Install Xcode Commandline Tools"
+  xcode-select --install
+
+  notice "Install Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  notice "Install the softwares via Homebrew"
+  brew bundle --file "$ROOT_PATH/dotfiles_mac/.Brewfile"
 }
 
 setup_linux() {
